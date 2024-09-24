e9e607b6bc84f66b817a5c0cc2066e7b943b88fb
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-09-24 22:32:08 +0530

    Create sign-up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import {SignUp} from "@/pages/sign-up.tsx";
 
 function App() {
   return (
     <>
+      <div className="bg-neutral-50 dark:bg-neutral-950">
+        <Routes>
+          <Route path="/" element={<SignUp/>}/>
 
+          <Route path="*" element={<SignUp/>}/>
+        </Routes>
+      </div>
     </>
   )
 }
